9f984c219d3ffd4c72204ee43d11956502482d76
Amirmohamad A <amirmohamada017@gmail.com>
2024-05-15 05:26:42 +0330

    add searching and sorting to product api.

--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from django.db.models import Count
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
+from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Product, Collection, OrderItem, Review
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
@@ -10,8 +11,11 @@ from .filters import ProductFilter
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
     filterset_class = ProductFilter
+    search_fields = ['title', 'description']
+    ordering_fields = ['unit_price', 'last_update']
+
 
     def get_serializer_context(self):
         return {'request': self.request}
